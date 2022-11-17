31a8c65abd217db4e7bbc6bd11b770018766e32c
buttaRahul <butta.rahul.aditya@gmail.com>
2022-11-17 22:52:24 +0530

    modifed state to mange hsitory of game'

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,32 @@ import './styles/root.scss';
 const App = () => {
   const [boardState,setBoardState] = useState(Array(9).fill(null));
   const [isSameUser,setIsSameUser] = useState(true);
-  // console.log(boardState);
-  const winner = calculateWinner(boardState);
-  const message = winner ? `Winner is ${winner}` : `Next Palyer is ${isSameUser ? 'X' : 'O'}`;
+
+  const [history , setHistory] = useState([{board:Array(9).fill(null),isSameUser:true}])
+  const[currentMove,setCurrentMove] = useState(0)
+  const current = history[currentMove]
+
+  const winner = calculateWinner(current.board);
+  const message = winner ? `Winner is ${winner}` : `Next Palyer is ${current.isSameUser ? 'X' : 'O'}`;
+
   const handleSquareClick = pos=>{
-    if(boardState[pos] || winner) return;
-    setBoardState(prev=>{
-      return prev.map((value,index)=>{
-        if(index==pos) return isSameUser ? 'X' : 'O';
-        return value;
-      })
+    if(current.board[pos] || winner) return;
+    setHistory(prev=>{
+       const last = prev[prev.length - 1]
+       const newBoard = last.board.map((square,index)=>{
+        if(pos == index)  return last.isSameUser ? 'X' : 'O';
+        return square;
+       })
+       return prev.concat({board:newBoard,isSameUser:!last.isSameUser})
     })
-    setIsSameUser(prev=> !prev)
-  }
+    setCurrentMove(prev => prev + 1);
+   }
 
   return (
     <div className='app'>
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board boardState={boardState} handleSquareClick={handleSquareClick}/>
+      <Board boardState={current.board} handleSquareClick={handleSquareClick}/>
     </div>
   );
 };
